0d9b0ba6f1ad3aa1294433f5019b3f75bbb3fef1
Robert Grimwade <79865756+pobertt@users.noreply.github.com>
2026-03-23 09:54:41 +0000

    publish

---
+++ RTBase-main/RTBase/Geometry.h
@@ -0,0 +1,263 @@
+#pragma once
+
+#include "Core.h"
+#include "Sampling.h"
+
+class Ray
+{
+public:
+	Vec3 o;
+	Vec3 dir;
+	Vec3 invDir;
+	Ray()
+	{
+	}
+	Ray(Vec3 _o, Vec3 _d)
+	{
+		init(_o, _d);
+	}
+	void init(Vec3 _o, Vec3 _d)
+	{
+		o = _o;
+		dir = _d;
+		invDir = Vec3(1.0f / dir.x, 1.0f / dir.y, 1.0f / dir.z);
+	}
+	Vec3 at(const float t) const
+	{
+		return (o + (dir * t));
+	}
+};
+
+class Plane
+{
+public:
+	Vec3 n;
+	float d;
+	void init(Vec3& _n, float _d)
+	{
+		n = _n;
+		d = _d;
+	}
+	// Add code here
+	bool rayIntersect(Ray& r, float& t)
+	{
+		// Change this so it uses the built in dot product
+		float denom = (n.x * r.dir.x) + (n.y * r.dir.y) + (n.z * r.dir.z);
+		float dotno = (n.x * r.o.x) + (n.y * r.o.y) + (n.z * r.o.z);
+		float numerator = -(dotno + d);
+		t = numerator / denom;
+
+		if (t < 0.0f) {
+			return false;
+		}
+		return true;
+	}
+};
+
+#define EPSILON 0.001f
+
+class Triangle
+{
+public:
+	Vertex vertices[3];
+	Vec3 e1; // Edge 1
+	Vec3 e2; // Edge 2
+	Vec3 n; // Geometric Normal
+	float area; // Triangle area
+	float d; // For ray triangle if needed
+	unsigned int materialIndex;
+	void init(Vertex v0, Vertex v1, Vertex v2, unsigned int _materialIndex)
+	{
+		materialIndex = _materialIndex;
+		vertices[0] = v0;
+		vertices[1] = v1;
+		vertices[2] = v2;
+		e1 = vertices[2].p - vertices[1].p;
+		e2 = vertices[0].p - vertices[2].p;
+		n = e1.cross(e2).normalize();
+		area = e1.cross(e2).length() * 0.5f;
+		d = Dot(n, vertices[0].p);
+	}
+	Vec3 centre() const
+	{
+		return (vertices[0].p + vertices[1].p + vertices[2].p) / 3.0f;
+	}
+	// Add code here
+	bool rayIntersect(const Ray& r, float& t, float& u, float& v) const
+	{
+		// Bayocentric Coords
+		float denom = Dot(n, r.dir);
+		if (denom == 0) { return false; }
+		t = (d - Dot(n, r.o)) / denom;
+		if (t < 0) { return false; }
+		Vec3 p = r.at(t);
+		float invArea = 1.0f / Dot(e1.cross(e2), n);
+		u = Dot(e1.cross(p - vertices[1].p), n) * invArea;
+		if (u < 0 || u > 1.0f) { return false; }
+		v = Dot(e2.cross(p - vertices[2].p), n) * invArea;
+		if (v < 0 || (u + v) > 1.0f) { return false; }
+		return true;
+		
+		/*
+		// Moller Trumbore
+		Vec3 edge1 = vertices[1].p - vertices[0].p;
+		Vec3 edge2 = vertices[2].p - vertices[0].p;
+		Vec3 p = r.dir.cross(edge2);
+		if (edge1.dot(p) < 0.000001f) { return false; };
+		float invdet = 1.0f / edge1.dot(p);
+
+		Vec3 T = r.o - vertices[0].p;
+		u = T.dot(p) * invdet;
+		if (u < 0.0f || u > 1.0f) { return false; };
+
+		Vec3 q = T.cross(edge1);
+		v = r.dir.dot(q) * invdet;
+		if (v < 0.0f || u + v > 1.0f) { return false; }
+
+		t = edge2.dot(q) * invdet;
+
+		return true;
+		*/
+	}
+	void interpolateAttributes(const float alpha, const float beta, const float gamma, Vec3& interpolatedNormal, float& interpolatedU, float& interpolatedV) const
+	{
+		interpolatedNormal = vertices[0].normal * alpha + vertices[1].normal * beta + vertices[2].normal * gamma;
+		interpolatedNormal = interpolatedNormal.normalize();
+		interpolatedU = vertices[0].u * alpha + vertices[1].u * beta + vertices[2].u * gamma;
+		interpolatedV = vertices[0].v * alpha + vertices[1].v * beta + vertices[2].v * gamma;
+	}
+	// Add code here
+	Vec3 sample(Sampler* sampler, float& pdf)
+	{
+		return Vec3(0, 0, 0);
+	}
+	Vec3 gNormal()
+	{
+		return (n * (Dot(vertices[0].normal, n) > 0 ? 1.0f : -1.0f));
+	}
+};
+
+class AABB
+{
+public:
+	Vec3 max;
+	Vec3 min;
+	AABB()
+	{
+		reset();
+	}
+	void reset()
+	{
+		max = Vec3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+		min = Vec3(FLT_MAX, FLT_MAX, FLT_MAX);
+	}
+	void extend(const Vec3 p)
+	{
+		max = Max(max, p);
+		min = Min(min, p);
+	}
+	// Add code here
+	bool rayAABB(const Ray& r, float& t)
+	{
+		Vec3 Tmin = (min - r.o) * r.invDir;
+		Vec3 Tmax = (max - r.o) * r.invDir;
+		Vec3 Tentry = Min(Tmin, Tmax);
+		Vec3 Texit = Max(Tmin, Tmax);
+		float tentry = std::max(Tentry.x, std::max(Tentry.y, Tentry.z));
+		float texit = std::min(Texit.x, std::min(Texit.y, Texit.z));
+		t = std::min(tentry, texit);
+		return (tentry <= texit && texit > 0);
+
+	}
+	// Add code here
+	bool rayAABB(const Ray& r)
+	{
+		Vec3 s = (min - r.o) * r.invDir;
+		Vec3 l = (max - r.o) * r.invDir;
+		Vec3 s1 = Min(s, l);
+		Vec3 l1 = Max(s, l);
+		float ts = std::max(s1.x, std::max(s1.y, s1.z));
+		float tl = std::min(l1.x, std::min(l1.y, l1.z));
+		return (ts <= tl && tl > 0);
+	}
+	// Add code here
+	float area()
+	{
+		Vec3 size = max - min;
+		return ((size.x * size.y) + (size.y * size.z) + (size.x * size.z)) * 2.0f;
+	}
+};
+
+class Sphere
+{
+public:
+	Vec3 centre;
+	float radius;
+	void init(Vec3& _centre, float _radius)
+	{
+		centre = _centre;
+		radius = _radius;
+	}
+	// Add code here
+	bool rayIntersect(Ray& r, float& t)
+	{
+		return false;
+	}
+};
+
+struct IntersectionData
+{
+	unsigned int ID;
+	float t;
+	float alpha;
+	float beta;
+	float gamma;
+};
+
+#define MAXNODE_TRIANGLES 8
+#define TRAVERSE_COST 1.0f
+#define TRIANGLE_COST 2.0f
+#define BUILD_BINS 32
+
+class BVHNode
+{
+public:
+	AABB bounds;
+	BVHNode* r;
+	BVHNode* l;
+	// This can store an offset and number of triangles in a global triangle list for example
+	// But you can store this however you want!
+	// unsigned int offset;
+	// unsigned char num;
+	BVHNode()
+	{
+		r = NULL;
+		l = NULL;
+	}
+	// Note there are several options for how to implement the build method. Update this as required
+	void build(std::vector<Triangle>& inputTriangles, std::vector<Triangle>& outputTriangles)
+	{
+		// marks for getting the BVH working and then more marks for it to be better (impress tom, get more marks)
+		// Add BVH building code here
+
+
+	}
+	void traverse(const Ray& ray, const std::vector<Triangle>& triangles, IntersectionData& intersection)
+	{
+		// marks for getting the BVH working and then more marks for it to be better (impress tom, get more marks)
+		// Add BVH Traversal code here
+	}
+	IntersectionData traverse(const Ray& ray, const std::vector<Triangle>& triangles)
+	{
+		IntersectionData intersection;
+		intersection.t = FLT_MAX;
+		traverse(ray, triangles, intersection);
+		return intersection;
+	}
+
+	bool traverseVisible(const Ray& ray, const std::vector<Triangle>& triangles, const float maxT)
+	{
+		// Add visibility code here
+		return true;
+	}
+};
---
+++ RTBase-main/RTBase/Main.cpp
@@ -0,0 +1,169 @@
+#include "GEMLoader.h"
+#include "Renderer.h"
+#include "SceneLoader.h"
+#define NOMINMAX
+#include "GamesEngineeringBase.h"
+#include <unordered_map>
+#include "Geometry.h"
+
+void triRayIntersectTest() {
+	Ray r{ Vec3(0,10,0), Vec3(0,-1,0) };
+
+	Vertex v1; v1.p = Vec3(0, 0, 0);
+	Vertex v2; v2.p = Vec3(2.0, 0, 0);
+	Vertex v3; v3.p = Vec3(0, 2.0, 0);
+	Triangle tri;
+	tri.init(v1, v2, v3, 0);
+
+	float t = 0.0f;
+	float u = 0.0f;
+	float v = 0.0f;
+
+	r.o = Vec3(0.5f, 0.5f, 5.0f);
+	r.dir = Vec3(0.0f, 0.0f, -1.0f);
+
+	bool hit = tri.rayIntersect(r, t, u, v);
+
+	if (hit) {
+        std::cout << "hit, dist (t): " << t << " u: " << u << " v: " << v << "\n";
+    } else {
+        std::cout << "miss\n";
+    }
+}
+
+void planeRayIntersectTest() {
+	// Add test code here
+	Ray r{ Vec3(0,10,0), Vec3(0,-1,0) };
+	Plane p{ Vec3(0, 1, 0), 0.0f };
+	float hitdist = 0.0f;
+	bool hit = p.rayIntersect(r, hitdist);
+
+	std::cout << "Plane Ray Intersect Test: " << hit << std::endl;
+	if (hit) {
+		std::cout << "Hit Distance: " << hitdist << std::endl;
+	}
+}
+
+void runTests()
+{
+	//StriRayIntersectTest();
+}
+
+int main(int argc, char *argv[])
+{
+	// Add call to tests if required
+	runTests();
+	
+	// Initialize default parameters
+	std::string sceneName = "cornell-box";
+	std::string filename = "GI.hdr";
+	unsigned int SPP = 8192;
+
+	if (argc > 1)
+	{
+		std::unordered_map<std::string, std::string> args;
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string arg = argv[i];
+			if (!arg.empty() && arg[0] == '-')
+			{
+				std::string argName = arg;
+				if (i + 1 < argc)
+				{
+					std::string argValue = argv[++i];
+					args[argName] = argValue;
+				} else
+				{
+					std::cerr << "Error: Missing value for argument '" << arg << "'\n";
+				}
+			} else
+			{
+				std::cerr << "Warning: Ignoring unexpected argument '" << arg << "'\n";
+			}
+		}
+		for (const auto& pair : args)
+		{
+			if (pair.first == "-scene")
+			{
+				sceneName = pair.second;
+			}
+			if (pair.first == "-outputFilename")
+			{
+				filename = pair.second;
+			}
+			if (pair.first == "-SPP")
+			{
+				SPP = stoi(pair.second);
+			}
+		}
+	}
+	Scene* scene = loadScene(sceneName);
+	GamesEngineeringBase::Window canvas;
+	canvas.create((unsigned int)scene->camera.width, (unsigned int)scene->camera.height, "Tracer", false);
+	RayTracer rt;
+	rt.init(scene, &canvas);
+	bool running = true;
+	GamesEngineeringBase::Timer timer;
+	while (running)
+	{
+		canvas.checkInput();
+		canvas.clear();
+		if (canvas.keyPressed(VK_ESCAPE))
+		{
+			break;
+		}
+		if (canvas.keyPressed('W'))
+		{
+			viewcamera.forward();
+			rt.clear();
+		}
+		if (canvas.keyPressed('S'))
+		{
+			viewcamera.back();
+			rt.clear();
+		}
+		if (canvas.keyPressed('A'))
+		{
+			viewcamera.left();
+			rt.clear();
+		}
+		if (canvas.keyPressed('D'))
+		{
+			viewcamera.right();
+			rt.clear();
+		}
+		if (canvas.keyPressed('E'))
+		{
+			viewcamera.flyUp();
+			rt.clear();
+		}
+		if (canvas.keyPressed('Q'))
+		{
+			viewcamera.flyDown();
+			rt.clear();
+		}
+		// Time how long a render call takes
+		timer.reset();
+		rt.render();
+		float t = timer.dt();
+		// Write
+		std::cout << t << std::endl;
+		if (canvas.keyPressed('P'))
+		{
+			rt.saveHDR(filename);
+		}
+		if (canvas.keyPressed('L'))
+		{
+			size_t pos = filename.find_last_of('.');
+			std::string ldrFilename = filename.substr(0, pos) + ".png";
+			rt.savePNG(ldrFilename);
+		}
+		if (SPP == rt.getSPP())
+		{
+			rt.saveHDR(filename);
+			break;
+		}
+		canvas.present();
+	}
+	return 0;
+}
